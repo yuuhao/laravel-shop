f92d1531c667a3b68085047d8e81828a28002734
vagrant <vagrant@homestead>
2018-08-03 09:37:30 +0000

    前端

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,9 @@ let mix = require('laravel-mix');
  |
  */
 
+//在末尾加了一个 version()，使 Mix 每次生成的静态文件后面加上一个类似版本号的参数，
+// 避免浏览器缓存
+
 mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+   .sass('resources/assets/sass/app.scss', 'public/css')
+   .version();
